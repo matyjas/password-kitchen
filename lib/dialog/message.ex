99af86afe536fd441167fdbd5526b80acabfd8ab
Maciej Matyjas <matyjas@gmail.com>
2018-06-05 07:03:44 +0100

    pulled message interface from my magic project

---
+++ lib/dialog/message.ex
@@ -0,0 +1,12 @@
+defmodule Dialog.Message do
+
+  @moduledoc """
+  Generic message behaviour, abstracts platform specifics
+  """
+
+  @type helpful_response :: {:ok, String.t} | {:error, String.t}
+
+  @callback extract_sender_id(any) :: helpful_response
+  @callback extract_date(any) :: helpful_response
+  @callback extract_utterance(any) :: helpful_response
+end
