1bfebe65d537a7587e5204ebbc09e9702c9ef74a
Maciej Matyjas <matyjas@gmail.com>
2018-06-07 06:50:02 +0100

    mix formatted

--- lib/dialog/message.ex
+++ lib/dialog/message.ex
@@ -1,10 +1,9 @@
 defmodule Dialog.Message do
-
   @moduledoc """
   Generic message behaviour, abstracts platform specifics
   """
 
-  @type helpful_response :: {:ok, String.t} | {:error, String.t}
+  @type helpful_response :: {:ok, String.t()} | {:error, String.t()}
 
   @callback extract_sender_id(any) :: helpful_response
   @callback extract_date(any) :: helpful_response
