b9b96c65b65231fcf8665c7258a57899d28dc2d4
Maciej Matyjas <matyjas@gmail.com>
2018-06-29 06:35:00 +0100

    on a journey to mocking the gateway

--- test/dialog/convo_test.exs
+++ test/dialog/convo_test.exs
@@ -24,4 +24,13 @@ defmodule Dialog.ConvoTest do
     conversation = Convo.get_messages(pid)
     assert conversation == [message]
   end
+
+  test "sends passwords" do
+    {:ok, pid} = Convo.start_link([])
+    message = "test-message"
+    parser = SimpleMessage
+    gateway = MockGateway
+    
+    Convo.put_message(pid, message, parser, gateway)
+  end
 end
