5d9c59163943f1ede0afbebfd4afe01fcbaa69ba
Maciej Matyjas <matyjas@gmail.com>
2018-07-12 07:28:40 +0100

    a struct for passwords

--- lib/kitchen/password.ex
+++ lib/kitchen/password.ex
@@ -3,9 +3,16 @@ defmodule Kitchen.Password do
   container for passwords
   """
 
-  alias __MODULE__
+#  alias __MODULE__
 
-  defstruct password: "", length: 0,
-    nums?: false, caps?: false, symbols?: false, lowers?: false
+  @enforce_keys [:password, :size, :nums?, :caps?, :symbols?, :lowers?]
+  defstruct password: "", size: 0, nums?: false,
+    caps?: false, symbols?: false, lowers?: false
   
+end
+
+defimpl String.Chars, for: Kitchen.Password do
+  def to_string(password) do
+    "#{String.replace(password.password, ~r/./, "*")} has length of #{password.size}."
+  end
 end
