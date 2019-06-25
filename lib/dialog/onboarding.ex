754e22493a0f88d0412212c091089c5ac31f6594
Maciej Matyjas <matyjas@users.noreply.github.com>
2019-06-25 07:11:48 +0100

    Improve onboarding messages
    Slight edits to onboarding messages

--- lib/dialog/onboarding.ex
+++ lib/dialog/onboarding.ex
@@ -3,8 +3,8 @@ defmodule Dialog.Onboarding do
   Supports onboarding text
   """
   @greetings {"Welcome to Password Kitchen, we offer delicious passwords @ reasonable rates.",
-              "Password Kitchen is an authentic and family run restaurant specialising in high quality passwords for modern customers",
-              "Open 24 hours a day for take away passwords of the highest quality",
+              "Password Kitchen is an authentic, family run restaurant specialising in high quality passwords for modern customers",
+              "Open 24 hours a day for take-away passwords of the highest quality",
   "Free refills on standard size passwords"}
 
   def get(timestamp), do: @greetings |> elem(rem(timestamp, 4))
