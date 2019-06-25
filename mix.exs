22d731345d10d3651292c7dc92bbfc77a05706d2
Maciej Matyjas <matyjas@users.noreply.github.com>
2019-06-25 07:29:49 +0100

    Update to plug and cowboy v 2
    previously on cowboy 1.1 and plug 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PK.MixProject do
   def project do
     [
       app: :password_kitchen,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -25,11 +25,10 @@ defmodule PK.MixProject do
     [
       {:credo, "~> 0.9.1", only: [:dev]},
       {:dialyxir, "~> 1.0.0-rc.2", only: [:dev], runtime: false},
-      {:cowboy, "~> 1.1"},
-      {:plug, "~> 1.4"},
       {:jason, "~> 1.1"},
       {:httpotion, "~> 3.1.0"},
-      {:mox, "~> 0.3", only: :test}
+      {:mox, "~> 0.3", only: :test},
+      {:plug_cowboy, "~> 2.0"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
