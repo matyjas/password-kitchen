15cc1a438b46c874b46c2672e44adb091babd19a
Maciej Matyjas <matyjas@users.noreply.github.com>
2019-07-02 07:16:54 +0100

    addressed warnings
    protocol wasnt fully implemented in test module

--- test/support/dialog/fake_gateway.ex
+++ test/support/dialog/fake_gateway.ex
@@ -5,12 +5,13 @@ defmodule Support.Dialog.FakeGateway do
   A gateway that simply writes to console
   """
 
-  def send_message(_, _), do: %HTTPotion.Response{}
+  def start_link(_), do: {:ok, self()}
+  def send_message(_, _, _), do: %HTTPotion.Response{}
 
-  def send_password(_, password) do
+  def send_password(_, _, password) do
     IO.puts(password)
     %HTTPotion.Response{}
   end
 
-  def send_onboarding(_, _), do: %HTTPotion.Response{}
+  def send_onboarding(_, _, _, _), do: %HTTPotion.Response{}
 end
