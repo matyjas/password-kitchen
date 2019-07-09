95d3f879427e647d024351840ff5a588790843aa
Maciej Matyjas <matyjas@users.noreply.github.com>
2019-07-09 07:18:32 +0100

    add documentation for Gateway behaviour

--- lib/dialog/gateway.ex
+++ lib/dialog/gateway.ex
@@ -6,7 +6,9 @@ defmodule Dialog.Gateway do
   """
 
   @typedoc """
-  really should not expose HTTPotion over here, leaky abstraction, 
+  the full HTTP response
+
+  really should not expose `t:HTTPotion.Response.t/0` over here, leaky abstraction, 
   what if we change HTTP libraries? 
   """
   @type response :: HTTPotion.Response.t()
@@ -16,14 +18,32 @@ defmodule Dialog.Gateway do
   @callback start_link(GenServer.option()) :: GenServer.on_start()
 
   @doc """
-  send a plain text message to the end user.
+  send a plain text message to an end user.
 
-  `pid` is the process id
-  `to_id` is the id of the end user to whom we send the message
-  `text` is the body of the message
+  * `pid` is the process id
+  * `to_id` is the id of the end user to whom we send the message
+  * `text` is the body of the message
   """
   @callback send_message(pid, String.t(), String.t()) :: response
+
+  @doc """
+  send a password to an end user.
+
+  * `pid` is the process id
+  * `to_id` is the id of the end user to whom we send the message
+  * `password` is the `Kitchen.Password` struct containing a password
+  """
   @callback send_password(pid, String.t(), %Password{}) :: response
+
+  @doc """
+  send an onboarding message to an end user.
+
+  onboarding messages will contain a password so we give value to users ASAP.
+  * `pid` is the process id
+  * `to_id` is the id of the end user to whom we send the message
+  * `text` is the onboarding text
+  * `password` is the `Kitchen.Password` struct containing a password
+  """
   # credo:disable-for-next-line Credo.Check.Readability.MaxLineLength
   @callback send_onboarding(pid, String.t(), String.t(), %Password{}) :: response
 end
